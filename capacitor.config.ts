0c0099face8ce8e72dae0627873440e297d78b74
Francisco Salinas <javiersalinas161@gmail.com>
2023-08-04 14:00:52 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tercer_Parcial_IPDM',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
